a975f42eb4b00e2076dc14374ce56affa3fb70ff
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-21 17:39:15 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,28 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts(__uv$config.sw || '/uv/uv.sw.js');
+importScripts('/dynamic/dynamic.config.js');
+importScripts('/dynamic/dynamic.worker.js');
+importScripts('dist/uv.bundle.js');
+importScripts('dist/uv.config.js');
+importScripts(__uv$config.sw || 'dist/uv.sw.js');
 
-const sw = new UVServiceWorker();
+const uv = new UVServiceWorker();
+const dynamic = new Dynamic();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.dynamic = dynamic;
+
+self.addEventListener('fetch',
+    event => {
+        event.respondWith(
+            (async function() {
+                if (await dynamic.route(event)) {
+                    return await dynamic.fetch(event);
+                }
+
+                if (event.request.url.startsWith(location.origin + "/service/uv/")) {
+                    return await uv.fetch(event);
+                }
+
+                return await fetch(event.request);
+            })()
+        );
+    }
+);
