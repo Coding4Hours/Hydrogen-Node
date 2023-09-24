e46f2a419260f3380f7b3ec69e86da7b21f622cd
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-24 15:56:02 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -31,7 +31,6 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  var proxified = __uv$config.prefix + __uv$config.encodeUrl(url);
-  frame.src = proxified;
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
 
