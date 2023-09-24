1aee731583a7950c47f129fe252b1fc342de5dba
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-24 15:38:25 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -32,6 +32,6 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
 
