31f6f208a9d3430de6377aec022a054c9af8833a
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-24 09:58:22 -0700

    Update index.js

--- public/browser/index.js
+++ public/browser/index.js
@@ -1,20 +1,3 @@
-window.addEventListener('load', function() {
-var search = document.getElementById("search");
-search.addEventListener('keyup', function onEvent(e) {
-    if (e.keyCode === 13) {
-        go()
-    }
-});
-})
-
-function go() {
-    var search = document.getElementById("search")
-    if (search.value !== "") {
-    window.location.href = "/service/gateway?url=" + search.value
-    search.value = ""
-    }
-}
-
 document.addEventListener('keydown', function(e){
 //Ctrl + Y: New Tab
 if (e.ctrlKey && e.keyCode == 89) {
