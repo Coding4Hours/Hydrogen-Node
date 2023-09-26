0b37717ba9b27dfdbb146cda31eead64dc7483b9
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-26 16:57:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const bare = createBareServer("/bare/");
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 var publicPath = __dirname + "/public"
 
 // Load our publicPath first and prioritize it over UV.
