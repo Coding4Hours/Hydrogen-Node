8cdbfb5230cc20231b74f061cfbd40747599eaf6
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-27 21:21:11 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -32,6 +32,5 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  var iframe = document.createElement('iframe');
-  iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);;
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);;
 });
