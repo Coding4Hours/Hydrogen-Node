1a3933377a1c092f201d1d1876338a87c3029ca2
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-28 17:39:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { join, dirname } from "node:path";
 import { hostname } from "node:os";
 import { fileURLToPath } from "url";
+import { rateLimit } from 'express-rate-limit'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const bare = createBareServer("/bare/");
 const app = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	// store: ... , // Use an external store for more precise rate limiting
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 var publicPath = __dirname + "/public"
 
