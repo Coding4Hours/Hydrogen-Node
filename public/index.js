3214226700536b340116474dd428d6a9c99f790d
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-29 16:08:46 -0700

    Test

--- public/index.js
+++ public/index.js
@@ -32,5 +32,51 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);;
+  //test start
+
+  var iframe = document.createElement('iframe');
+
+            iframe.style.position = "absolute";
+            iframe.style.width = "100%";
+            iframe.style.height = "100%";
+            iframe.style.top = "0px";
+            iframe.style.left = "0px";
+            iframe.id = "iframe";
+            iframe.style.zIndex="9999999999999999";
+            iframe.style.border = "none";
+            iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+            document.body.appendChild(iframe);
+
+            var x = document.createElement('img');
+            x.style.cursor="pointer";
+            x.style.position = "absolute";
+            x.style.width = "50px";
+            x.style.height = "50px";
+            x.src = "/img/x.png";
+            x.style.zIndex = "99999999999999999999";
+            x.style.right = "1%";
+            x.style.top = "1%";
+            x.onclick = function() {
+                window.location.reload(1);
+            };
+
+            document.body.appendChild(x);
+
+
+            var open = document.createElement('img');
+            open.style.cursor="pointer";
+            open.style.position = "absolute";
+            open.style.width = "50px";
+            open.style.height = "50px";
+            open.src = "/img/open.png";
+            open.style.zIndex = "99999999999999999999";
+            open.style.right = "65px";
+            open.style.top = "1%";
+            open.onclick = function() {
+                if (document.getElementById("iframe").contentWindow.location.href!="about:blank"){
+                    window.open(document.getElementById("iframe").contentWindow.location.href);
+                }
+            document.body.appendChild(open);
+              //end test
+//location.href = __uv$config.prefix + __uv$config.encodeUrl(url);;
 });
