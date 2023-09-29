80b1605ae49a9bca86199ac5e57395a8311ce4ee
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-29 16:17:28 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -46,5 +46,36 @@ form.addEventListener("submit", async (event) => {
             iframe.style.border = "none";
             iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
             document.body.appendChild(iframe);
+
+              var x = document.createElement('img');
+            x.style.cursor="pointer";
+            x.style.position = "absolute";
+            x.style.width = "50px";
+            x.style.height = "50px";
+            x.src = "https://ima.minecraftpotato.com/img/x.png";
+            x.style.zIndex = "99999999999999999999";
+            x.style.right = "1%";
+            x.style.top = "1%";
+            x.onclick = function() {
+                window.location.reload(1);
+            };
+
+            document.body.appendChild(x);
+
+
+            var open = document.createElement('img');
+            open.style.cursor="pointer";
+            open.style.position = "absolute";
+            open.style.width = "50px";
+            open.style.height = "50px";
+            open.src = "https://ima.minecraftpotato.com/img/open.png";
+            open.style.zIndex = "99999999999999999999";
+            open.style.right = "65px";
+            open.style.top = "1%";
+            open.onclick = function() {
+                if (document.getElementById("iframe").contentWindow.location.href!="about:blank"){
+                    window.open(document.getElementById("iframe").contentWindow.location.href);
+                }
+            document.body.appendChild(open);
 //location.href = __uv$config.prefix + __uv$config.encodeUrl(url);;
 });
