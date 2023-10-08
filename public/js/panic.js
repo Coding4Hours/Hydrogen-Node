33ef9f6a0fee7b9dd1d88706e315ec6b5fc458ef
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-08 08:48:48 -0700

    Update panic.js

--- public/js/panic.js
+++ public/js/panic.js
@@ -1,5 +1,5 @@
-var eventKey = localStorage.getItem("eventKey") || prompt("What panic key do you want?"");
-var panicLink = localStorage.getItem("panicLink") || prompt("What panic link do you want?"";
+var eventKey = localStorage.getItem("eventKey") || prompt('What panic key do you want?');
+var panicLink = localStorage.getItem("panicLink") || prompt('What panic link do you want?');
 document.addEventListener("keydown", function(event) {
   if (event.key === eventKey) {
     if (window.self !== window.top) {
